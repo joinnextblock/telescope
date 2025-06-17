e60703fede0ba8790388256185daa1583b166050
Ian Reis <484095+onesien@users.noreply.github.com>
2025-06-17 11:42:13 -0400

    Release: t-v0.0.4 (#6)

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# @joinnextblock/telescope
+
+## 0.0.4
+
+### Patch Changes
+
+- 6ff3e0a: fix lunar calculation errors
+
+## 0.0.3
+
+### Patch Changes
+
+- e408d2a: implement basic telescope library
+- a94cfda: add github workflow for versioning package
--- src/lib/lunar/index.test.ts
+++ src/lib/lunar/index.test.ts
@@ -1,6 +1,7 @@
 import { create_lunar } from "./index";
 
 describe("Lunar", () => {
+  const blockheight = 901632;
   it("should throw an error if the blockheight is not an integer", () => {
     expect(() => create_lunar(undefined)).toThrow("Blockheight must be an integer");
   });
@@ -9,79 +10,64 @@ describe("Lunar", () => {
   });
 
   it("should create a Lunar object with valid input", () => {
-    const lunarInstance = create_lunar(0);
-    expect(lunarInstance.get_blockheight()).toBe(0);
+    const lunarInstance = create_lunar(blockheight);
+    expect(lunarInstance.get_blockheight()).toBe(blockheight);
   });
 
   describe("phase", () => {
     it("should return correct lunar phase index", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_phase_index()).toBe(0);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_phase_index()).toBe(4);
     });
 
     it("should return correct lunar phase index", () => {
-      const lunarInstance = create_lunar(1);
-      expect(lunarInstance.get_phase_block_height()).toBe(1);
-    });
-    it("should return correct lunar phase index", () => {
-      const lunarInstance = create_lunar(505);
-      expect(lunarInstance.get_phase_index()).toBe(1);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_phase_block_height()).toBe(blockheight % 504);
     });
 
     it("should return correct next lunar phase index", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_next_phase_index()).toBe(1);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_next_phase_index()).toBe(5);
     });
 
     it("should return correct blocks until next lunar phase", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_blocks_until_next_phase()).toBe(504);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_blocks_until_next_phase()).toBe(24);
     });
-    it("should return correct blocks until next lunar phase", () => {
-      const lunarInstance = create_lunar(504);
-      expect(lunarInstance.get_blocks_until_next_phase()).toBe(504);
+
+    it("should return correct lunar phase", () => {
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_phase().name).toBe("new");
+    });
+    it("should return correct next lunar phase", () => {
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_next_phase().name).toBe("waxing crescent");
     });
   });
   describe("cycle", () => {
     it("should return correct lunar cycle index", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_cycle_index()).toBe(0);
-    });
-    it("should return correct lunar cycle index", () => {
-      const lunarInstance = create_lunar(505);
-      expect(lunarInstance.get_cycle_index()).toBe(1);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_cycle_index()).toBe(2);
     });
     it("should return correct blocks until next lunar cycle", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_blocks_until_next_cycle()).toBe(4032);
-    });
-    it("should return correct blocks until next lunar cycle", () => {
-      const lunarInstance = create_lunar(505);
-      expect(lunarInstance.get_blocks_until_next_cycle()).toBe(3527);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_blocks_until_next_cycle()).toBe(1536);
     });
     it("should return correct lunar cycle block height", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_cycle_block_height()).toBe(0);
-    });
-    it("should return correct lunar cycle block height", () => {
-      const lunarInstance = create_lunar(505);
-      expect(lunarInstance.get_cycle_block_height()).toBe(505);
-    });
-    it("should return correct lunar cycle block height", () => {
-      const lunarInstance = create_lunar(4032);
-      expect(lunarInstance.get_cycle_block_height()).toBe(0);
-    });
-    it("should return correct lunar cycle block height", () => {
-      const lunarInstance = create_lunar(4033);
-      expect(lunarInstance.get_cycle_block_height()).toBe(1);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_cycle_block_height()).toBe(2496);
     });
     it("should return correct next lunar cycle index", () => {
-      const lunarInstance = create_lunar(0);
-      expect(lunarInstance.get_next_cycle_index()).toBe(1);
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_next_cycle_index()).toBe(3);
     });
-    it("should return correct next lunar cycle index", () => {
-      const lunarInstance = create_lunar(4032);
-      expect(lunarInstance.get_next_cycle_index()).toBe(1);
+    it("should return correct lunar cycle", () => {
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_cycle().name).toBe("Friend");
+    });
+    it("should return correct next lunar cycle", () => {
+      const lunarInstance = create_lunar(blockheight);
+      expect(lunarInstance.get_next_cycle().name).toBe("Whale");
     });
   })
 });
